7aba8a4870f6eeafcd426b415e687ba6e51bfdf8
Nathan Janke <nathanjanke96@gmail.com>
2025-09-24 18:11:22 -0700

    Essentials script

---
+++ scripts/essentials.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# Update and Upgrade
+sudo apt update && sudo apt upgrade
+
+# QEMU Guest Agent
+sudo apt install -y qemu-guest-agent
+
+# Essential Packages
+sudo apt install -y neovim \
+  htop
+
+# Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+sudo usermod -aG docker $(whoami)
+
+# dtop
+curl -sSfL https://amir20.github.io/dtop/install.sh | bash
+
+echo ""
+echo "---"
+echo "Done installing the essentials, don't forget to reboot"
